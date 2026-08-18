b05c824f8aa0ddd5f86e7b8a0a444c41b3442488
bayanahalharbi-ui <bayan.ah.alharbi@gmail.com>
2026-08-18 12:01:46 +0300

    Delete assets directory

--- assets/firebase-config.js
+++
@@ -1,8 +0,0 @@
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyB3n7SvC8RXGDoy69vLYpA-FR2XhjSqwhs",
-  authDomain: "haij-a2591.firebaseapp.com",
-  projectId: "haij-a2591",
-  storageBucket: "haij-a2591.firebasestorage.app",
-  messagingSenderId: "289121297134",
-  appId: "1:289121297134:web:d6cbcedfbeeaa475f489ff"
-};
